00d8de6471c9e08ec7682c10810d5578554f7ed2
Shreya070223 <ankook0987@gmail.com>
2025-08-01 23:53:27 +0530

    added some feature in authlayout

--- frontend/vite-project/src/pages/Auth/Login.jsx
+++ frontend/vite-project/src/pages/Auth/Login.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 import AuthLayout from '../../components/layouts/authLayout'
+import {useNavigate} from 'react-router-dom'
 
 const Login = () => {
+  const [email,setEmail]=useState("");
+  const [passward,setPassward]=useState("");
+  const [error,setError]=useState(null);
+
+  const nevigate=useNavigate();
   return (
     <AuthLayout>
       <div className='lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center'>
@@ -9,6 +15,12 @@ const Login = () => {
         <p className='text-xs text-slate-700 mt-[5px] mb-6'>
           Please enter your details to log in
         </p>
+
+       <form onSubmit={handleLogin}>
+
+       </form>
+
+
       </div>
     </AuthLayout>
   )
