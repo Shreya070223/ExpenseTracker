f6ad580a6c3b77188d6aa290f779df3374c139a3
Shreya070223 <ankook0987@gmail.com>
2025-08-09 16:06:41 +0530

    setup the frontend

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,37 +1,13 @@
-import React from 'react'
-import {BrowserRouter as Router,Routes,Route,Navigate} from "react-router-dom";
-
-import Login from './pages/Auth/Login';
-import SignUp from './pages/Auth/SignUp';
-import Home from './pages/Dashboard/Home';
-import Expense from './pages/Dashboard/Expense';
-import Income from './pages/Dashboard/Income';
+import { useState } from 'react'
 
 
-const App = () => {
+function App() {
+  
   return (
-    <div className='text-2xl'>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Root/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/signUp" element={<SignUp/>}/>
-          <Route path="/dashboard" element={<Home/>}/>
-          <Route path="/income" element={<Income/>}/>
-          <Route path="/expense" element={<Expense/>}/>
-        </Routes>
-      </Router>
-    </div>
+    <>
+      
+    </>
   )
 }
 
-export default App;
-
-const Root=()=>{
-  const isAuthenticated=!!localStorage.getItem("token");
-  return isAuthenticated?(
-    <Navigate to="/dashboard"/>
-  ):(
-    <Navigate to="/login"/>
-  );
-};
+export default App
